581d8c451a78d35b800db200bc4d2bfba198e92c
Gustavo-F <fgustavo016@gmail.com>
2021-12-03 21:04:38 -0300

    Adicionado model para categorias de produtos

--- loja/admin.py
+++ loja/admin.py
@@ -2,14 +2,22 @@ from . import models
 from django.contrib import admin
 
 
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'nome')
+    list_display_links = ('pk', 'nome')
+
+
+admin.site.register(models.Categoria, CategoriaAdmin)
+
+
 class ProdutoFotoInline(admin.TabularInline):
     model = models.ProdutoFoto
     extra = 0
 
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'nome', 'quantidade', 'preco')
-    list_display_links = ('pk', 'nome', 'quantidade', 'preco')
+    list_display = ('pk', 'nome', 'quantidade', 'preco', 'preco_promocional', 'categoria')
+    list_display_links = ('pk', 'nome', 'quantidade', 'preco', 'preco_promocional', 'categoria')
 
     inlines = [ProdutoFotoInline]
 
