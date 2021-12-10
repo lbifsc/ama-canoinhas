9de50ce4fc7719eeb6269119c99ccc79cee12039
gustavopirro <gustavo.pirro@gmail.com>
2021-12-10 20:00:53 -0300

    modificado layout loja geral

--- loja/urls.py
+++ loja/urls.py
@@ -7,5 +7,5 @@ app_name = 'loja'
 urlpatterns = [
     # Loja
     path('', view=views.LojaGeral.as_view(), name='loja_geral'),
-    path('/item/', view=views.LojaItem.as_view(), name='loja_item'),
+    path('item/', view=views.LojaItem.as_view(), name='loja_item'),
 ]
